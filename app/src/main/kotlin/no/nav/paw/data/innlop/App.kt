480e60a2e42708b47b6478b2cdab1b10df7d6b82
Jostein Holje <jostein.holje@nav.no>
2023-02-15 13:46:18 +0100

    Bruk riktige topicer

--- app/src/main/kotlin/no/nav/paw/data/innlop/App.kt
+++ app/src/main/kotlin/no/nav/paw/data/innlop/App.kt
@@ -4,8 +4,8 @@ import no.nav.paw.data.innlop.kafka.TopicProducer.Companion.dataTopic
 import no.nav.paw.data.innlop.tjenester.AutomatiskReaktiveringTjeneste
 
 fun main() {
-    val automatiskReaktiveringTopic = System.getenv("AUTOMATISK_REAKTIVERING_TOPIC")
-    val automatiskReaktiveringSvarTopic = System.getenv("AUTOMATISK_REAKTIVERING_SVAR_TOPIC")
+    val dataReaktiveringTopic = System.getenv("DATA_REAKTIVERING_TOPIC")
+    val dataReaktiveringSvarTopic = System.getenv("DATA_REAKTIVERING_SVAR_TOPIC")
 
-    AutomatiskReaktiveringTjeneste(dataTopic<AutomatiskReaktivering>(automatiskReaktiveringTopic), dataTopic<AutomatiskReaktiveringSvar>(automatiskReaktiveringSvarTopic)).start()
+    AutomatiskReaktiveringTjeneste(dataTopic<AutomatiskReaktivering>(dataReaktiveringTopic), dataTopic<AutomatiskReaktiveringSvar>(dataReaktiveringSvarTopic)).start()
 }
