7750ec19d2bce13ed0efd6bbc78003c6e5681e54
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-02-16 12:22:28 +0100

    r: fjerner melding fra logging og setter deserialiserings exception klasse

--- app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
+++ app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
@@ -4,7 +4,6 @@ import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import no.nav.common.kafka.util.KafkaPropertiesPreset
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsConfig
-import org.apache.kafka.streams.StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG
 import org.apache.kafka.streams.errors.LogAndFailExceptionHandler
 import java.util.*
 
@@ -15,7 +14,7 @@ data class Config(
             put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().javaClass)
             put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().javaClass)
             put(StreamsConfig.APPLICATION_ID_CONFIG, System.getenv("KAFKA_CONSUMER_GROUP_ID"))
-            put(DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, LogAndFailExceptionHandler::class.java)
+            put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, LogAndFailExceptionHandler::class.java)
         },
     val schemaRegistry: Map<String, String> = mapOf(
         KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG to System.getenv("KAFKA_SCHEMA_REGISTRY"),
