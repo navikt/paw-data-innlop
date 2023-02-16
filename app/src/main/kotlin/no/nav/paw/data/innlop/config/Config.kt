f59ebce9680753ed5cad26b37d30170e733f319e
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-02-16 10:10:16 +0100

    r: legger til application id

--- app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
+++ app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
@@ -3,6 +3,7 @@ package no.nav.paw.data.innlop.config
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import no.nav.common.kafka.util.KafkaPropertiesPreset
+import org.apache.kafka.streams.StreamsConfig
 import java.util.*
 
 data class Config(
@@ -17,6 +18,7 @@ data class Config(
                 "${System.getenv("KAFKA_SCHEMA_REGISTRY_USER")}:${System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")}"
             )
             put(KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
+            put(StreamsConfig.APPLICATION_ID_CONFIG, System.getenv("KAFKA_CONSUMER_GROUP_ID"))
         },
     val schemaRegistry: Map<String, String> = mapOf(
         AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to System.getenv("KAFKA_SCHEMA_REGISTRY"),
