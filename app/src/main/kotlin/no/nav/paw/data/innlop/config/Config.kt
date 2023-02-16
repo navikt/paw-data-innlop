34c38a1a14c80fe4f02c53870631c9b551a6981e
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-02-16 10:26:59 +0100

    r: forsøker kafka streams config

--- app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
+++ app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
@@ -3,21 +3,16 @@ package no.nav.paw.data.innlop.config
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import no.nav.common.kafka.util.KafkaPropertiesPreset
+import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsConfig
 import java.util.*
 
 data class Config(
     val kafka: Properties = KafkaPropertiesPreset
         .aivenDefaultProducerProperties(System.getenv("KAFKA_CONSUMER_GROUP_ID")).apply {
-            put(
-                KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
-                System.getenv("KAFKA_SCHEMA_REGISTRY")
-            )
-            put(
-                KafkaAvroSerializerConfig.USER_INFO_CONFIG,
-                "${System.getenv("KAFKA_SCHEMA_REGISTRY_USER")}:${System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")}"
-            )
-            put(KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
+            put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().javaClass)
+            put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().javaClass)
             put(StreamsConfig.APPLICATION_ID_CONFIG, System.getenv("KAFKA_CONSUMER_GROUP_ID"))
         },
     val schemaRegistry: Map<String, String> = mapOf(
