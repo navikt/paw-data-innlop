1f771a3a67b37a94ff7768a5a5fa9fbfc90c3c5f
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-02-16 11:41:28 +0100

    r: fjerner duplikat setting av kafka bootstrap server

--- app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
+++ app/src/main/kotlin/no/nav/paw/data/innlop/config/Config.kt
@@ -1,6 +1,5 @@
 package no.nav.paw.data.innlop.config
 
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import no.nav.common.kafka.util.KafkaPropertiesPreset
 import org.apache.kafka.common.serialization.Serdes
@@ -16,7 +15,6 @@ data class Config(
             put(StreamsConfig.APPLICATION_ID_CONFIG, System.getenv("KAFKA_CONSUMER_GROUP_ID"))
         },
     val schemaRegistry: Map<String, String> = mapOf(
-        AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to System.getenv("KAFKA_SCHEMA_REGISTRY"),
         KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG to System.getenv("KAFKA_SCHEMA_REGISTRY"),
         KafkaAvroSerializerConfig.USER_INFO_CONFIG to "${System.getenv("KAFKA_SCHEMA_REGISTRY_USER")}:${System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")}",
         KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE to "USER_INFO"
